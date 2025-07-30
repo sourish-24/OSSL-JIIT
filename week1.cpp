d744769beae6ff56bf7f80d498b65022ca1ff054
sourish-24 <157837444+sourish-24@users.noreply.github.com>
2025-07-30 11:20:50 +0530

    Update week1.c

--- week1.cpp
+++ week1.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int x= 10;
-    cout<<x;
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0;
+
+    printf("Enter a 3-digit number: ");
+    scanf("%d", &num);
+
+    if (num >= 100 && num <= 999) {
+        int hundreds = num / 100;
+        int tens = (num / 10) % 10;
+        int units = num % 10;
+
+        reversed = units * 100 + tens * 10 + hundreds;
+
+        printf("Reversed number: %d\n", reversed);
+    } else {
+        printf("Invalid input! Please enter a 3-digit number.\n");
+    }
+
     return 0;
 }
